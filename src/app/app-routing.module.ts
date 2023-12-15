d157e65ac59aab00b8d95022c29c6de18d041f19
Emir Mustafoski <emirmustafoski92@gmail.com>
2023-12-15 14:58:33 +0100

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ResultsComponent } from './results/results.component';
+import { DetailsComponent } from './details/details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'results/:query', component: ResultsComponent },
+  { path: 'details/:id', component: DetailsComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
